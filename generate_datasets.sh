df2fe0c2c7f594b13015fb95824dfa2a9a026998
Sergey Lavrushkin <dualfal@gmail.com>
2018-07-04 16:41:24 +0300

    Changes 'python' to 'python3' in .sh scripts

--- generate_datasets.sh
+++ generate_datasets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python datasets/load_harmonic_video.py --save_folder=datasets/loaded_harmonic --cut_logo
-python datasets/load_div2k.py --save_folder=datasets/loaded_div2k
+python3 datasets/load_harmonic_video.py --save_folder=datasets/loaded_harmonic --cut_logo
+python3 datasets/load_div2k.py --save_folder=datasets/loaded_div2k
 
-python datasets/prepare_dataset.py --video_folder=datasets/loaded_harmonic --dataset_folder=datasets/train --type=blocks --temporal_radius=1 --frames_per_scene=7 --block_size=36 --stride=36
-python datasets/prepare_div2k_dataset.py --div2k_folder=datasets/loaded_div2k/train --dataset_folder=datasets/train_div2k --type=blocks
-python datasets/prepare_div2k_dataset.py --div2k_folder=datasets/loaded_div2k/test --dataset_folder=datasets/test_div2k --type=full
+python3 datasets/prepare_dataset.py --video_folder=datasets/loaded_harmonic --dataset_folder=datasets/train --type=blocks --temporal_radius=1 --frames_per_scene=7 --block_size=36 --stride=36
+python3 datasets/prepare_div2k_dataset.py --div2k_folder=datasets/loaded_div2k/train --dataset_folder=datasets/train_div2k --type=blocks
+python3 datasets/prepare_div2k_dataset.py --div2k_folder=datasets/loaded_div2k/test --dataset_folder=datasets/test_div2k --type=full
--- train_eval.sh
+++ train_eval.sh
@@ -9,19 +9,19 @@ train_eval () {
             else
                 CKPT_OPTION="--ckpt_path=$TRAINING_LOGDIR/model_$i.ckpt"
             fi
-            python train.py --logdir=$TRAINING_LOGDIR --dataset_path=$TRAINING_DATASET_PATH --dataset_info_path=$TRAINING_DATASET_INFO_PATH --num_epochs=$2 $CKPT_OPTION --epochs_per_save=$2 --steps_per_log=$STEPS_PER_LOG --learning_rate=$LEARNING_RATE $USE_LR_DECAY_FLAG $STAIRCASE_LR_DECAY_FLAG --lr_decay_rate=$LR_DECAY_RATE --lr_decay_epochs=$LR_DECAY_EPOCHS --model=$MODEL --optimizer=$OPTIMIZER --batch_size=$BATCH_SIZE --shuffle_buffer_size=$SHUFFLE_BUFFER_SIZE $USE_MC $MC_INDEPENDENT
+            python3 train.py --logdir=$TRAINING_LOGDIR --dataset_path=$TRAINING_DATASET_PATH --dataset_info_path=$TRAINING_DATASET_INFO_PATH --num_epochs=$2 $CKPT_OPTION --epochs_per_save=$2 --steps_per_log=$STEPS_PER_LOG --learning_rate=$LEARNING_RATE $USE_LR_DECAY_FLAG $STAIRCASE_LR_DECAY_FLAG --lr_decay_rate=$LR_DECAY_RATE --lr_decay_epochs=$LR_DECAY_EPOCHS --model=$MODEL --optimizer=$OPTIMIZER --batch_size=$BATCH_SIZE --shuffle_buffer_size=$SHUFFLE_BUFFER_SIZE $USE_MC $MC_INDEPENDENT
             ret=$?
             if [ $ret -ne 0 ]; then
                 break
             fi
         
-            python evaluate.py --logdir=$EVAL_LOGDIR --dataset_path=$TESTING_DATASET_PATH --dataset_info_path=$TESTING_DATASET_INFO_PATH --ckpt_path=$TRAINING_LOGDIR --model=$MODEL $USE_MC
+            python3 evaluate.py --logdir=$EVAL_LOGDIR --dataset_path=$TESTING_DATASET_PATH --dataset_info_path=$TESTING_DATASET_INFO_PATH --ckpt_path=$TRAINING_LOGDIR --model=$MODEL $USE_MC
             ret=$?
             if [ $ret -ne 0 ]; then
                 break
             fi
         done
     else
-        python train.py --logdir=$TRAINING_LOGDIR --dataset_path=$TRAINING_DATASET_PATH --dataset_info_path=$TRAINING_DATASET_INFO_PATH --num_epochs=$1 $CKPT_OPTION --epochs_per_save=$2 --steps_per_log=$STEPS_PER_LOG --learning_rate=$LEARNING_RATE $USE_LR_DECAY_FLAG $STAIRCASE_LR_DECAY_FLAG --lr_decay_rate=$LR_DECAY_RATE --lr_decay_epochs=$LR_DECAY_EPOCHS --model=$MODEL --optimizer=$OPTIMIZER --batch_size=$BATCH_SIZE --shuffle_buffer_size=$SHUFFLE_BUFFER_SIZE $USE_MC $MC_INDEPENDENT
+        python3 train.py --logdir=$TRAINING_LOGDIR --dataset_path=$TRAINING_DATASET_PATH --dataset_info_path=$TRAINING_DATASET_INFO_PATH --num_epochs=$1 $CKPT_OPTION --epochs_per_save=$2 --steps_per_log=$STEPS_PER_LOG --learning_rate=$LEARNING_RATE $USE_LR_DECAY_FLAG $STAIRCASE_LR_DECAY_FLAG --lr_decay_rate=$LR_DECAY_RATE --lr_decay_epochs=$LR_DECAY_EPOCHS --model=$MODEL --optimizer=$OPTIMIZER --batch_size=$BATCH_SIZE --shuffle_buffer_size=$SHUFFLE_BUFFER_SIZE $USE_MC $MC_INDEPENDENT
     fi
 }
